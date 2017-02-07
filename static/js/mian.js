7a0e1475ef695c91ae9cd25761d8c50acf8b47b2
Sam.Shen <samfec@163.com>
2017-02-07 18:03:17 +0800

    .

---
+++ static/js/mian.js
@@ -0,0 +1,10 @@
+import '../sass/main.css';
+import '../sass/test.scss';
+
+
+// let img2 = document.createElement("img");
+// img2.src = require("../images/Icon180.png");
+// document.querySelector(".app").appendChild(img2);
+
+
+import './test.js';
